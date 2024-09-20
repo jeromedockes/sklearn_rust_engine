70d44d820e3473ee87faafe08c637dc60b9889cf
Auguste Baum <auguste@probabl.ai>
2024-09-20 14:44:23 +0200

    back to basics

--- rust/src/lib.rs
+++ rust/src/lib.rs
@@ -1,19 +1,12 @@
-use numpy::ndarray::{ArrayD,  ArrayView2,ArrayViewD, Axis};
-use std::iter::Enumerate;
-use std::ops::{Sub, Mul};
 use numpy::ndarray::ArrayBase;
+use numpy::ndarray::{ArrayD, ArrayView2, ArrayViewD, Axis};
 use numpy::{
-    IntoPyArray,  PyArrayDyn,
-    PyReadonlyArrayDyn, PyReadonlyArray2, PyReadonlyArray1, PyReadwriteArray2,
-    PyReadwriteArray1,
-    NotContiguousError
+    IntoPyArray, NotContiguousError, PyArrayDyn, PyReadonlyArray1, PyReadonlyArray2,
+    PyReadonlyArrayDyn, PyReadwriteArray1, PyReadwriteArray2,
 };
-use pyo3::{
-    pymodule,
-    types::PyModule,
-    Bound, PyResult, Python,
-};
-
+use pyo3::{pymodule, types::PyModule, Bound, PyResult, Python};
+use std::iter::Enumerate;
+use std::ops::{Mul, Sub};
 
 #[pymodule]
 fn sklearn_rust_engine<'py>(m: &Bound<'py, PyModule>) -> PyResult<()> {
@@ -36,90 +29,132 @@ fn sklearn_rust_engine<'py>(m: &Bound<'py, PyModule>) -> PyResult<()> {
         IntoPyArray::into_pyarray_bound(z, py)
     }
 
+    //     def lloyd_iter_chunked_dense(
+    //             const floating[:, ::1] X,            # IN
+    //             const floating[::1] sample_weight,   # IN
+    //             const floating[:, ::1] centers_old,  # IN
+    //             floating[:, ::1] centers_new,        # OUT
+    //             floating[::1] weight_in_clusters,    # OUT
+    //             int[::1] labels,                     # OUT
+    //             floating[::1] center_shift,          # OUT
+    //             int n_threads,
+    //             bint update_centers=True):
 
-//     def lloyd_iter_chunked_dense(
-//             const floating[:, ::1] X,            # IN
-//             const floating[::1] sample_weight,   # IN
-//             const floating[:, ::1] centers_old,  # IN
-//             floating[:, ::1] centers_new,        # OUT
-//             floating[::1] weight_in_clusters,    # OUT
-//             int[::1] labels,                     # OUT
-//             floating[::1] center_shift,          # OUT
-//             int n_threads,
-//             bint update_centers=True):
+    #[pyfn(m)]
+    // #[pyo3(name = "truc")]
+    fn lloyd_iter_chunked_dense(
+        X: PyReadonlyArray2<f64>,
+        centers_old: PyReadonlyArray2<f64>,
+        mut centers_new: PyReadwriteArray2<f64>,
+        mut labels: PyReadwriteArray1<i64>,
+        mut center_shift: PyReadwriteArray1<f64>,
+        update_centers: bool,
+    ) -> PyResult<()> {
+        let X_view: ArrayView2<f64> = X.as_array();
+        let centers_view: ArrayView2<f64> = centers_old.as_array();
 
-     #[pyfn(m)]
-     // #[pyo3(name = "truc")]
-     fn lloyd_iter_chunked_dense(
-         X: PyReadonlyArray2<f64>,
-         centers_old: PyReadonlyArray2<f64>,
-         mut centers_new: PyReadwriteArray2<f64>,
-         mut labels: PyReadwriteArray1<i64>,
-         mut center_shift: PyReadwriteArray1<f64>,
-         update_centers: bool
-     ) -> PyResult<()> {
+        for (sample_idx, sample) in X_view.axis_iter(Axis(0)).enumerate() {
+            // println!("sample {sample_idx}\n============================");
+            let mut closest_center_idx = 0;
+            let mut smallest_squared_dist = f64::INFINITY;
+            for (idx, center) in centers_view.axis_iter(Axis(0)).enumerate() {
+                let diff = sample.sub(&center);
+                let squared = (&diff).mul(&diff);
+                let squared_dist = squared.sum();
+                // println!("squared dist: {}", squared_dist);
+                if squared_dist < smallest_squared_dist {
+                    smallest_squared_dist = squared_dist;
+                    closest_center_idx = idx;
+                    // println!("closest centroid: {closest_center_idx}");
+                }
+            }
+            (*labels.get_mut(sample_idx).unwrap()) = closest_center_idx as i64;
+            // Process each row here
+            // For example, print the row
+        }
+        // dbg!("{}", labels);
 
-         let X_view: ArrayView2<f64> = X.as_array();
-         let centers_view: ArrayView2<f64> = centers_old.as_array();
-
-         for (sample_idx, sample) in X_view.axis_iter(Axis(0)).enumerate() {
-             // println!("sample {sample_idx}\n============================");
-             let mut closest_center_idx = 0;
-             let mut smallest_squared_dist = f64::INFINITY;
-             for (idx, center) in centers_view.axis_iter(Axis(0)).enumerate(){
-                 let diff = sample.sub(&center);
-                 let squared = (&diff).mul(&diff);
-                 let squared_dist = squared.sum();
-                 // println!("squared dist: {}", squared_dist);
-                 if squared_dist < smallest_squared_dist {
-                     smallest_squared_dist = squared_dist;
-                     closest_center_idx = idx;
-                     // println!("closest centroid: {closest_center_idx}");
-                 }
-             }
-             (*labels.get_mut(sample_idx).unwrap()) = closest_center_idx as i64;
-             // Process each row here
-             // For example, print the row
-         }
-         // dbg!("{}", labels);
-
-        let first_item = centers_new.get_mut([0,0]).unwrap();
+        let first_item = centers_new.get_mut([0, 0]).unwrap();
         *first_item = 100.;
 
-         centers_new
+        centers_new
             .as_slice_mut()
             .unwrap()
             .iter_mut()
-            .for_each(|x| {*x = *x + 1.;});
+            .for_each(|x| {
+                *x = *x + 1.;
+            });
 
-         centers_new
+        centers_new
             .as_slice_mut()
             .unwrap()
             .iter_mut()
-            .map(|x| {*x = *x + 1.;})
+            .map(|x| {
+                *x = *x + 1.;
+            })
             .count();
 
-         let _ = centers_new
+        let _ = centers_new
             .as_slice_mut()
             .unwrap()
             .iter_mut()
-            .map(|x| {*x = *x + 1.;})
+            .map(|x| {
+                *x = *x + 1.;
+            })
             .collect::<()>();
 
-         let centers_new_slice = centers_new.as_slice_mut().unwrap();
-         for item in centers_new_slice {
-             *item = *item + 1. ;
-         }
+        let centers_new_slice = centers_new.as_slice_mut().unwrap();
+        for item in centers_new_slice {
+            *item = *item + 1.;
+        }
 
-         Ok(())
+        Ok(())
     }
 
     /// Formats the sum of two numbers as string.
     #[pyfn(m)]
     #[pyo3()]
     fn my_sum_as_string(a: usize, b: usize) -> PyResult<String> {
-         Ok((a + b).to_string())
+        Ok((a + b).to_string())
     }
 
-   Ok(())
+    Ok(())
+}
+
+#[derive(Debug, PartialEq)]
+struct KMeansResult {
+    cluster_centroids: Vec<Vec<f64>>,
+    // For each point in x, which cluster it is assigned to
+    cluster_assignments: Vec<i64>,
+}
+
+fn kmeans(x: Vec<Vec<f64>>, n_clusters: i64) -> KMeansResult {
+    KMeansResult {
+        cluster_centroids: vec![],
+        cluster_assignments: vec![],
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn it_works() {
+        // let x = vec![vec![0.0, 0.0], vec![1.0, 1.0]];
+        let x = vec![
+            vec![0.0, 0.0],
+            vec![0.5, 0.0],
+            vec![0.5, 1.0],
+            vec![1.0, 1.0],
+        ];
+        let result = kmeans(x, 2);
+        assert_eq!(
+            result,
+            KMeansResult {
+                cluster_centroids: vec![vec![148.], vec![2.]],
+                cluster_assignments: vec![],
+            }
+        );
+    }
 }
--- src/tests/test_all.py
+++ src/tests/test_all.py
@@ -3,4 +3,4 @@ import sklearn_rust_engine
 
 
 def test_sum_as_string():
-    assert sklearn_rust_engine.sum_as_string(1, 1) == "2"
+    assert sklearn_rust_engine.my_sum_as_string(1, 1) == "2"
